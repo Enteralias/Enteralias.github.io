9765b77b254838a998cf67904fb41536d35bcc82
Yannick Gras <pro@ygras.fr>
2025-08-15 21:35:00 +0200

    Try fix3

--- js/global.js
+++ js/global.js
@@ -48,7 +48,7 @@ if (!openImageBtn || !modal || !closeBtn) {
     };
 }
 
-showCompetencies();
+// showCompetencies();
 
 
 function zoomCompetencies() {
@@ -271,6 +271,7 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('Container articles-preview non trouvé !');
   }
 });
+
 
 
 
